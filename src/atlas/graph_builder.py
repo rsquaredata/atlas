b54f7618cf6f667ee48938629e26c4e6dc5f56ee
Rina <contact.rina@icloud.com>
2026-06-11 22:17:37 +0200

    Add edge for Microsoft investing in OpenAI

--- src/atlas/graph_builder.py
+++ src/atlas/graph_builder.py
@@ -1,1 +1,13 @@
+import networkx as nx
 
+Relationship(
+    source="Microsoft",
+    relation="invested_in",
+    target="OpenAI"
+)
+
+graph.add_edge(
+    "Microsoft",
+    "OpenAI",
+    relation="invested_in"
+)
